069f9de56638ba7f0abcce2be984d2cac43b30a6
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-03-10 13:09:34 -0500

    Explain method argument and return value for given strings

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,15 +9,24 @@
 
 
 "Hello World".include?("Hello")
-
+# include? method is called on the string object "Hello World". The method checks the string object for inclusion of the argument passed
+# Argument "Hello" is passed to the method.
+# Return value is "true"
 
 "Hello World".end_with?("Hello")
-
+# end_with? method is called on the string object "Hello World". The method checks whether the string object ends with the argument passed
+# Argument "Hello" is passed to the method.
+# Return value is "false"
 
 "Hello World".end_with?("rld")
-
+# end_with? method is called on the string object "Hello World". The method checks whether the string object ends with the argument passed
+# Argument "rld" is passed to the method.
+# Return value is "true"
 
 "Hello World".length
-
+# length method is called on the string object "Hello World". The method counts all characters in the string object. It does not accept an argument.
+# Return value is "11"
 
 "Hello World".size
+# size method is called on the string object "Hello World". The method counts all characters in the string object. It does not accept an argument.
+# Return value is "11"
